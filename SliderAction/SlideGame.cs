8aa25251feda40b25481f1a5b8f0932e56150381
name <you@gmail.com>
2019-11-28 11:49:18 +0900

    using delete
    (cherry picked from commit 285fe202e6a15cbf65536651ec6057e54fde6c0b)

--- SliderAction/SlideGame.cs
+++ SliderAction/SlideGame.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using System.Diagnostics;
-using Microsoft.Xna.Framework.Audio;
+using System.Collections.Generic;
 
 namespace SliderAction
 {
